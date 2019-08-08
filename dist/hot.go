71cf7733333faf65a84ccb446db93cc2c5114e68
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-08-07 20:30:31 -0500

    Add hot distribution

--- dist/hot.go
+++ dist/hot.go
@@ -1,5 +1,17 @@
 package dist
 
+import "math/rand"
+
 func hot(ven *Venta) {
 	ven.Ventas = 1
+	probability := rand.Intn(100)
+	if probability >= 0 && probability < 20 {
+		ven.Ventas = 20
+	}
+	if probability >= 20 && probability < 50 {
+		ven.Ventas = 30
+	}
+	if probability >= 60 && probability < 100 {
+		ven.Ventas = 40
+	}
 }
