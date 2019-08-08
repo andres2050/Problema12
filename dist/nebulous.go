dd5e78ed2286c3c69321e83ba0d0755aea7424fd
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-08-07 20:42:49 -0500

    Add nebulous dist

--- dist/nebulous.go
+++ dist/nebulous.go
@@ -1,5 +1,19 @@
 package dist
 
+import "math/rand"
+
 func nebulous(ven *Venta) {
-	ven.Ventas = 1
+	probability := rand.Intn(100)
+	if probability >= 0 && probability < 10 {
+		ven.Ventas = 10
+	}
+	if probability >= 10 && probability < 30 {
+		ven.Ventas = 20
+	}
+	if probability >= 30 && probability < 70 {
+		ven.Ventas = 30
+	}
+	if probability >= 70 && probability < 100 {
+		ven.Ventas = 40
+	}
 }
