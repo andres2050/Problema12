66213b333e3d8e1aff681084dffb97ec15d5988c
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-08-07 20:00:28 -0500

    Add stormy weathers

--- dist/stormy.go
+++ dist/stormy.go
@@ -1,1 +1,16 @@
 package dist
+
+import "math/rand"
+
+func stormy(ven *Venta) {
+	max := 25
+	x := rand.Intn(max)
+	if x <= 0 {
+		ven.Ventas = 0
+	} else if x >= max {
+		ven.Ventas = 25
+	} else {
+		density := x / max
+		ven.Ventas = int(x * density)
+	}
+}
