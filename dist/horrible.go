505cd9f152bed2b8d2a19b1a295ccf7892158b51
Andres Felipe Herrera Moreno <aherrera@truora.com>
2019-08-07 20:45:04 -0500

    Add horrible dist

--- dist/horrible.go
+++ dist/horrible.go
@@ -1,5 +1,13 @@
 package dist
 
+import "math/rand"
+
 func horrible(ven *Venta) {
-	ven.Ventas = 1
+	probability := rand.Intn(100)
+	if probability >= 0 && probability < 80 {
+		ven.Ventas = 0
+	}
+	if probability >= 80 && probability < 100 {
+		ven.Ventas = 1
+	}
 }
