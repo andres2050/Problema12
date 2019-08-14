1ec498f29f43fa26621389b7c9bb5103c1948afe
SantiagoRP7 <santiago.19988@gmail.com>
2019-08-13 22:49:07 -0500

    Prueba de medias para el generador aleatorio terminada adjuntable al informe

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/andres2050/Problema12/dist"
-)
+import "github.com/andres2050/Problema12/ran"
 
 func main() {
 
-	ven := dist.DistClima(90)
-	str, _ := json.Marshal(ven)
-	fmt.Println(string(str))
+	// ven := dist.DistClima(90)
+	// str, _ := json.Marshal(ven)
+	// fmt.Println(string(str))
+	// fmt.Printf("\n")
+	ran.PruebaDeMedias(100)
 }
--- ran/middle.go
+++ ran/middle.go
@@ -2,23 +2,59 @@ package ran
 
 import (
 	"crypto/rand"
+	"fmt"
+	"math"
 	"math/big"
 )
 
-func randCreator(n int) []int64 {
-	rands := []int64{}
-	for i := n; i < n; i++ {
-		rndBig, _ := rand.Int(rand.Reader, big.NewInt(100))
-		rnd := rndBig.Int64()
+//RandCreator aas
+func RandCreator(n int) []float64 {
+	rands := []float64{}
+	for i := 0; i < n; i++ {
+		rndBig, _ := rand.Int(rand.Reader, big.NewInt(int64(n)))
+		rnd := float64(rndBig.Int64()) / float64((n))
 		rands = append(rands, rnd)
 	}
+
 	return rands
 }
 
-func media(rands []int64) int64 {
-	sum := int64(0)
+//Media asas
+func Media(rands []float64) float64 {
+	sum := float64(0)
+
 	for i := 0; i < len(rands); i++ {
 		sum = sum + rands[i]
 	}
-	return sum
+	if len(rands) > 0 {
+		k := sum / float64(len(rands))
+		return k
+	}
+
+	return 0
+}
+
+//Varianza varianza
+func Varianza(rands []float64, middle float64) float64 {
+	x := middle
+	sum := 0.0
+	for i := 0; i < len(rands); i++ {
+		sum = math.Pow(rands[i]+x, 2)
+	}
+
+	return sum / float64(len(rands))
+}
+
+//PruebaDeMedias realiza la prueba de medidas para el generador usado.
+func PruebaDeMedias(numbers int) {
+	var rands = RandCreator(numbers)
+	var middle = Media(rands)
+	varian := Varianza(rands, middle)
+	limiteSuperior := middle + 1.96*(varian/(math.Sqrt(float64(numbers))))
+	limiteInferior := middle - 1.96*(varian/(math.Sqrt(float64(numbers))))
+	if middle < limiteSuperior {
+		if middle > limiteInferior {
+			fmt.Printf("Prueba media superada: %f > %f > %f", limiteSuperior, middle, limiteInferior)
+		}
+	}
 }
